225081644c7357b6e46b00b71b6cec59f5b12e08
balanced <vladislavherasymenko@gmail.com>
2023-02-08 21:19:04 +0200

    refactor(front): remove swc and cypress

--- packages/frontend/cypress/plugins/index.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-module.exports = (on, config) => {
-  if (config.testingType === 'component') {
-    const { startDevServer } = require('@cypress/webpack-dev-server');
-    const webpackConfig = require('../webpack.cypress.config');
-
-    on('dev-server:start', options => startDevServer({ options, webpackConfig }));
-  }
-};
