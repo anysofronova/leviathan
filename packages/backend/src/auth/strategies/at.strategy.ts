3abdfd78f64daa03ca3469e6333254fc1ff3f1f0
balanced <vladislavherasymenko@gmail.com>
2023-02-11 13:08:01 +0200

    fix: docker db problem

--- packages/backend/src/auth/strategies/at.strategy.ts
+++ packages/backend/src/auth/strategies/at.strategy.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-
-import * as env from 'env-var';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
 import { TPayload } from '../types';
@@ -11,7 +9,7 @@ export class AtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: env.get('AT_SECRET').asString(),
+      secretOrKey: process.env.AT_SECRET,
     });
   }
 
--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,16 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-
-import * as env from 'env-var';
-
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const logger = new Logger('Application');
 
 const initApp = async () => {
   const app = await NestFactory.create(AppModule);
-  const port = env.get('PORT').default(4200).asPortNumber();
+  const port = process.env.PORT ?? 5000;
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
