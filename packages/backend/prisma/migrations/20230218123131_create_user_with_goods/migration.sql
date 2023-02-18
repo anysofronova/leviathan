cc03452e20ca947d6a7472d218168308f3391047
balanced <vladislavherasymenko@gmail.com>
2023-02-18 14:33:59 +0200

    fix: migrations

--- packages/backend/prisma/migrations/20230218123131_create_user_with_goods/migration.sql
+++ packages/backend/prisma/migrations/20230218123131_create_user_with_goods/migration.sql
@@ -11,5 +11,16 @@ CREATE TABLE "users" (
     "role" TEXT NOT NULL DEFAULT 'USER'
 );
 
+-- CreateTable
+CREATE TABLE "good" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "name" TEXT NOT NULL,
+    "price" REAL NOT NULL,
+    "description" TEXT NOT NULL,
+    "ownerId" INTEGER NOT NULL,
+    CONSTRAINT "good_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
