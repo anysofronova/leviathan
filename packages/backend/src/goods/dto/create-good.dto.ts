cb4eb0a6b176db79a1f8954b10ddfd4d16a5e5e5
Anna <anysofronova@yandex.ru>
2023-02-21 11:55:42 +0300

    feat(goods): create good dto

--- packages/backend/src/goods/dto/create-good.dto.ts
+++ packages/backend/src/goods/dto/create-good.dto.ts
@@ -1,1 +1,43 @@
-export class CreateGoodDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateGoodDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  productImage: string;
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  additionalImages: string;
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+  @ApiProperty()
+  @IsNotEmpty()
+  price: number;
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  size: string;
+  @ApiProperty()
+  @IsNotEmpty()
+  ownerId: number;
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  colors: string;
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  details: string;
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  care: string;
+}
