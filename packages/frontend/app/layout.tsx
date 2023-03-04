05c8d5fb775cf476728a72e140f5256b5e62bdb8
balanced <vladislavherasymenko@gmail.com>
2023-03-04 15:26:34 +0200

    refactor: lang

---
+++ packages/frontend/app/layout.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import '#/styles/globals.css'
+
+import { ThemeProvider } from 'next-themes'
+import { PropsWithChildren } from 'react'
+import { Provider } from 'react-redux'
+
+import { store } from '#/shared/store'
+import { Footer, Header } from '#/widgets'
+
+import Head from './head'
+
+export default function RootLayout({ children }: PropsWithChildren) {
+  return (
+    <html lang='en'>
+      <Head></Head>
+      <body className='bg-white dark:bg-[#171923]'>
+        <ThemeProvider attribute='class'>
+          <Provider store={store}>
+            <Header />
+            <main className='w-full bg-white pt-[120px] dark:bg-[#171923] dark:text-white lg:pt-[72px]'>
+              <div className='mx-auto h-[auto] min-h-[100vh] max-w-[2460px]'>{children}</div>
+            </main>
+            <Footer />
+          </Provider>
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
