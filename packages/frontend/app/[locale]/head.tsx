86c559f5063197b59786930b78fd0363516a9680
myakushev <m.yakushev@gmail.com>
2023-03-06 20:00:10 +0200

    fix: icon

--- packages/frontend/app/[locale]/head.tsx
+++ packages/frontend/app/[locale]/head.tsx
@@ -5,7 +5,7 @@ const Head = () => {
       <link href='/favicon/apple-touch-icon.png' rel='apple-touch-icon' sizes='180x180' />
       <link href='/favicon/favicon-32x32.png' rel='icon' sizes='32x32' type='image/png' />
       <link href='/favicon/favicon-16x16.png' rel='icon' sizes='16x16' type='image/png' />
-      <link href='/favicon/favicon.ico' rel='shortcut icon' />
+      <link rel='shortcut icon' type='image/x-icon' href='favicon.ico' />
       <title>Leviathan</title>
       <meta
         name='description'
