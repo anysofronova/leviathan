58e03746cb942ffdcae07fea1ac192f5bd0d8015
balanced <vladislavherasymenko@gmail.com>
2023-03-22 23:47:21 +0200

    fix: seed generation

--- packages/backend/src/common/entities/goods/types/index.ts
+++ packages/backend/src/common/entities/goods/types/index.ts
@@ -3,6 +3,7 @@ import { Category } from '@prisma/client';
 export type GoodFilters = Partial<{
   category: keyof typeof Category;
   sort: 'trending-desc' | 'latest-desc' | 'price-asc' | 'price-desc';
+  designer: number;
 }>;
 
 export type TGoodFilters = {
