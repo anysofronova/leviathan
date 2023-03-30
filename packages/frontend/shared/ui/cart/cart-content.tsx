612c4ec8790e8dd8e8e34344441b28401ef9dd6b
myakushev <m.yakushev@gmail.com>
2023-03-30 15:50:44 +0300

    fix: cart

--- packages/frontend/shared/ui/cart/cart-content.tsx
+++ packages/frontend/shared/ui/cart/cart-content.tsx
@@ -12,13 +12,13 @@ export const CartContent = () => {
         <div className='flex flex-col'>
           {cartProducts.map(el => {
             return (
-              <div key={useId()} className='mb-2 border-b pb-5'>
+              <div key={useId()} className={`mb-2 pb-5 ${cartProducts.at(-1) === el ? '' : 'border-b'}`}>
                 <div className='mb-2 flex items-center justify-between'>
-                  <div className='mr-3 w-[135px] bg-purple-700'>
+                  <div className='mr-3 w-[100px] bg-purple-700'>
                     <img src={el.productImage} alt='img' className='w-full' />
                   </div>
-                  <div className='w-full text-left font-medium'>
-                    <h3 className='mb-0.5 block text-xl'>{el.name}</h3>
+                  <div className='w-full text-left text-sm font-medium'>
+                    <h3 className='mb-0.5 block'>{el.name}</h3>
                     <div className='mb-0.5 flex items-center'>
                       <span>Size:</span>
                       <div className='mx-0.5 flex min-h-[22px] min-w-[22px] items-center justify-center rounded-full border border-gray-400 p-0.5 text-[11px] font-black'>
@@ -53,7 +53,7 @@ export const CartContent = () => {
           })}
         </div>
       </div>
-      <div className='mt-auto flex h-[25%] flex-col justify-between border-t bg-white p-6 dark:bg-black'>
+      <div className='mt-auto flex h-[32%] flex-col justify-between border-t bg-white p-6 dark:bg-black'>
         <div className='border-b pb-3'>
           <div className='flex justify-between'>
             <span>Subtotal</span>
@@ -68,7 +68,7 @@ export const CartContent = () => {
             <span className='font-bold'>FREE</span>
           </div>
         </div>
-        <div className='flex justify-between font-bold'>
+        <div className='my-3 flex justify-between font-bold'>
           <span>Total</span>
           <span>0</span>
         </div>
